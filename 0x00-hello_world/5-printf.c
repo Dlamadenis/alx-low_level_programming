f25e82dd158eef3c79d7c07f21941b45346bc887
dlamadenis <denisemmanuel985@gmail.com>
2022-09-10 12:46:31 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
-/**main - main function
-  *
-  *return: return 0 everytime
+/**
+*main - main function
+*
+*
+*return: return 0 everytime
 */
-
 int main(void)
 {
 	printf(with proper grammar, but the outcome is a piece of art, \n);
