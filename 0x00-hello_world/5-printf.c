2a4723b1c39ffd350db64d4d960c0ebc4f03d2ac
dlamadenis <denisemmanuel985@gmail.com>
2022-09-10 12:36:57 -0700

    t5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/* main - main function */
+/**main - main function
+  *
+  *return: return 0 everytime
+*/
 
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art, %.\n);
-	return(0);
+	printf(with proper grammar, but the outcome is a piece of art, \n);
+	return (0);
 }
