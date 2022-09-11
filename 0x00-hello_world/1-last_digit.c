33aed31975eb79a2fa30e44b184e0768a87383e9
dlamadenis <denisemmanuel985@gmail.com>
2022-09-11 10:39:30 -0700

    last digit

---
+++ 0x00-hello_world/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - prints the last digit of random number
+*
+*Return: return 0
+*/
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+
+	if (last == 0)
+	{
+		printf("last digit of %d and is 0\n", n, last digit);
+	}
+	else
+	{
+		if (last > 5)
+		{
+			printf("last digit of %d is %d and is greater than 5\n", n, last);
+		}
+		else if (last < 6)
+		{
+			printf("last digit of %d is %d and is less than 6 and not 0\n", n, last);
+		}
+	}
+	return (0);
+}
